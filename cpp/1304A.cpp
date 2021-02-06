3adcbb4abc2c9787d2a68cdca636aa6e37648232
NeverQuitYourDayDream <78627242+NeverQuitYourDayDream@users.noreply.github.com>
2021-02-06 13:45:16 -0500

    Add files via upload

---
+++ cpp/1304A.cpp
@@ -0,0 +1,29 @@
+/*
+ *1304A. Two Rabbits
+ *
+ *Completed in 31 ms 0 kb
+ */
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int t;
+    cin >> t;
+    while (t --) {
+        int x, y, a, b;
+        cin >> x >> y >> a >> b;
+        int dxy = y - x;
+        
+        if (dxy % (a + b) == 0) {
+            cout << dxy / (a + b) << endl;
+        } 
+        else {
+            cout << -1 << endl;
+        }
+    }
+ 
+    return 0;
+}
