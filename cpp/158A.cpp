15de4d0ebf34ac55f35c85a76c407f3c5aa0d2d9
NeverQuitYourDayDream <78627242+NeverQuitYourDayDream@users.noreply.github.com>
2021-02-06 18:19:08 -0500

    Create 158A.cpp

---
+++ cpp/158A.cpp
@@ -0,0 +1,40 @@
+// 158A Next Round
+// Completed in 62 ms 0 kb
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    
+    int a;
+    
+    for (int i = 0; i < k; ++i)
+    {
+        cin >> a;
+        if (a == 0)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    
+    int b;
+    
+    for (int i = k; i < n; ++i)
+    {
+        cin >> b;
+        
+        if (b != a)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    
+    cout << n << endl;
+    return 0;
+}
