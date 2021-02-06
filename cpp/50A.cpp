c5d41b52c36efb319ace5efd500ac7350094ff21
NeverQuitYourDayDream <78627242+NeverQuitYourDayDream@users.noreply.github.com>
2021-02-06 18:17:27 -0500

    Create 50A.cpp

---
+++ cpp/50A.cpp
@@ -0,0 +1,16 @@
+// 50A Domino piling
+// Completed in 31 ms 0 kb
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    
+    int m, n;
+    cin >> m >> n;
+    
+    cout << m * n / 2;
+    
+    return 0;
+}
