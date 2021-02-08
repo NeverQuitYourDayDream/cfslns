14e2b0b41c097d3166ae081b7fa7312466ed568e
NeverQuitYourDayDream <78627242+NeverQuitYourDayDream@users.noreply.github.com>
2021-02-08 11:39:22 -0500

    Create 1339A.cpp

---
+++ cpp/1339A.cpp
@@ -0,0 +1,18 @@
+// 1339A Filling Diamonds
+// Completed in 31 ms 0 kb
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n;
+        cin >> n;
+        cout <<n << '\n';
+    }
+    return 0;
+}
