d65cc99f5afb9f24c28e81038273047b42c4a25e
NeverQuitYourDayDream <78627242+NeverQuitYourDayDream@users.noreply.github.com>
2021-02-13 19:49:17 -0500

    Update 7A.py
    spacing

--- python/7A.py
+++ python/7A.py
@@ -7,22 +7,22 @@ painted_ranks = 0
 painted_files = 0
  
 for i in range(8):
-  rank = input()
-  # the number of B in each rank (row)
-  painted = rank.count('B')
-  if painted == 8:
-    painted_ranks += 1
-  board.append(rank)
+    rank = input()
+    # the number of B in each rank (row)
+    painted = rank.count('B')
+    if painted == 8:
+        painted_ranks += 1
+    board.append(rank)
  
 # find the number of files
 for i in range(8):
-  x = 0
-  for j in range(8):
-    if board[j][i] == 'B':
-      x += 1
+    x = 0
+    for j in range(8):
+        if board[j][i] == 'B':
+            x += 1
   
-  if x == 8:
-    painted_files += 1
+    if x == 8:
+        painted_files += 1
     
 if painted_ranks == 8:
     print(8)
