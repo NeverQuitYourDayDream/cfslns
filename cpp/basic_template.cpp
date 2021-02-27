35c79a26626f5fc2056ec658f57b25a76ee9a564
NeverQuitYourDayDream <78627242+NeverQuitYourDayDream@users.noreply.github.com>
2021-02-27 13:35:45 -0500

    Create basic_template.cpp

---
+++ cpp/basic_template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <map>
+#include <iomanip>
+#define INF 2147483647
+#define LINF 9223372036854775807
+#define pii pair<int, int>
+#define ll long long
+#define pb push_back
+#define newl '\n'
+using namespace std;
